384a1ab0f340808b0a5780cd8b9076e681ab6f54
Lin Shi <linshi1768@gmail.com>
2023-02-14 18:17:01 -0700

    hire function

--- FNCD.java
+++ FNCD.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class FNCD {
-
+    final int maxSize = 3;
     double budget;
     double dailySales;
     int date;
@@ -16,34 +16,48 @@ public class FNCD {
     public FNCD(){
         this.budget = 500000;
         this.simTime = 30;
-        this.date = 1;
+        this.date = 0;
         this.internList = new ArrayList<>();
         this.mechanicsList = new ArrayList<>();
         this.salespeopleList = new ArrayList<>();
-        for(int i = 0; i < 3; i++){
+        for(int i = 0; i < maxSize; i++){
 
             internList.add(new Interns("I" + i));
             mechanicsList.add(new Mechanics("m"+ i));
             salespeopleList.add(new Salesperson("s" + i));
         }
-        for(int j = 0; j < 3; j ++){
-            System.out.println(internList.get(j).getName());
-        }
     }
     public void simulation(){
         while(date < simTime){
-
-            if(date % 7 != 0 ){
-
-            }
+            startDay();
+            System.out.println(date);
+            date ++;
         }
 
     }
     public void startDay(){
-
+        hire();
     }
 
     public void hire(){
+        if(internList.size() != 3){
+            int tempLength = internList.size();
+            for(int i = 0; i < maxSize-tempLength; i++){
+                internList.add(new Interns("I" + i));
+            }
+        }
+        if(mechanicsList.size() != 3){
+            int tempLength = mechanicsList.size();
+            for(int i = 0; i < maxSize-tempLength; i++){
+                mechanicsList.add(new Mechanics("M" + i ));
+            }
+        }
+        if(salespeopleList.size() != 3){
+            int tempLength = salespeopleList.size();
+            for(int i = 0; i < maxSize-tempLength; i++){
+                salespeopleList.add(new Salesperson("S" + i));
+            }
+        }
 
     }
 
