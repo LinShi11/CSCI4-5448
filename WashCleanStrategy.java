2c0459fe8823105801c91adba5df4cce6a1b19c3
AnuraginiSinha <ansi3987@colorado.edu>
2023-03-01 22:08:34 -0700

    more comments

--- WashCleanStrategy.java
+++ WashCleanStrategy.java
@@ -5,12 +5,12 @@ public class WashCleanStrategy implements WashStrategy {
     @Override
     public String wash(Interns intern, Vehicle car) {
 
-        Random random = new Random();
+        Random random = new Random(); //this is the randomized chance
         int chance = random.nextInt(100);
         if(chance < 5){
             return "dirty";
         } else if(chance < 35){
-            intern.setDailyBonus(intern.getDailyBonus() + car.getWashBonus(1));
+            intern.setDailyBonus(intern.getDailyBonus() + car.getWashBonus(1)); // intern will get bonus based on how sparlking car is in the outcome
             return "sparkling";
         } else{
             return "clean";
