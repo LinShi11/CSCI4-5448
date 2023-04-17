6eb0a25673facf9cf03dcced22017886b72a98c3
Lin Shi <linshi1768@gmail.com>
2023-04-17 11:44:29 -0600

    adding building count

--- Game.java
+++ Game.java
@@ -15,14 +15,15 @@ public class Game {
     Font titleFont = new Font("Times New Roman", Font.PLAIN, 90);
     Font normalFont = new Font("Times New Roman", Font.PLAIN, 28);
 
-    JPanel titlePanel, startButtonPanel, eventAnnouncerPanel, buildingButtonPanel, resourcesPanel;
+    JPanel titlePanel, startButtonPanel, eventAnnouncerPanel, buildingButtonPanel, resourcesPanel, buildingPanel;
     JLabel titleLabel;
     JButton startButton, hutButton, smokeHouseButton, minesButton, factoryButton, blacksmithButton, bucketButton, trapButton;
-    JTextArea announcer, resources;
+    JTextArea announcer, resources, buildings;
 
     TitleScreenHandler tsHandler = new TitleScreenHandler();
     ChoiceHandler choiceHandler = new ChoiceHandler();
     HashMap<String, Integer> resourceMap;
+    HashMap<String, Integer> buildingMap;
 
     public Game(){
         window = new JFrame();
@@ -40,6 +41,16 @@ public class Game {
         resourceMap.put("Fur", 0);
         resourceMap.put("Rock", 0);
         resourceMap.put("Clothes", 0);
+
+        buildingMap = new HashMap<>();
+        buildingMap.put("Trap", 0);
+        buildingMap.put("Bucket", 0);
+        buildingMap.put("Smokehouse", 0);
+        buildingMap.put("Factory", 0);
+        buildingMap.put("Blacksmith", 0);
+        buildingMap.put("Mines", 0);
+        buildingMap.put("Tradecart", 0);
+
 
         login();
 
@@ -87,12 +98,9 @@ public class Game {
 
         announcer = new JTextArea("This is the event Announcer");
         announcer.setBounds(100, 100, 200, 1000);
-        announcer.setBackground(Color.black);
-        announcer.setForeground(Color.white);
-        announcer.setFont(normalFont);
+        textColorHelper(announcer);
         announcer.setLineWrap(true);
         announcer.setWrapStyleWord(true);
-        announcer.setEditable(false);
 
         eventAnnouncerPanel.add(announcer);
 
@@ -118,25 +126,46 @@ public class Game {
         buttonHelper(trapButton, "trap");
 
         resourcesPanel = new JPanel();
-        resourcesPanel.setBounds(1000, 100, 400, 1000);
+        resourcesPanel.setBounds(1000, 100, 400, 300);
         resourcesPanel.setBackground(Color.BLACK);
 
         con.add(resourcesPanel);
         resources = new JTextArea();
 
-        resources.setBounds(1000,100,400,1000);
-        resources.setBackground(Color.black);
-        resources.setForeground(Color.white);
-        resources.setFont(normalFont);
+        resources.setBounds(1000,100,400,300);
 
         resources.setText("Resources \t Amount\n");
         for(Map.Entry<String, Integer> elements: resourceMap.entrySet()){
             resources.append(elements.getKey() + "\t" + elements.getValue() + "\n");
         }
-        resources.setEditable(false);
+        textColorHelper(resources);
         resourcesPanel.add(resources);
 
+        buildingPanel = new JPanel();
+        buildingPanel.setBounds(1000, 500, 400, 300);
+        buildingPanel.setBackground(Color.black);
 
+        con.add(buildingPanel);
+
+        buildings = new JTextArea();
+        buildings.setBounds(1000,500,400,300);
+
+        buildings.setText("Building\t Amount\n");
+        for(Map.Entry<String, Integer> elements: buildingMap.entrySet()){
+            buildings.append(elements.getKey() + "\t" + elements.getValue() + "\n");
+        }
+        textColorHelper(buildings);
+        buildingPanel.add(buildings);
+
+
+
+    }
+
+    public void textColorHelper(JTextArea area){
+        area.setForeground(Color.white);
+        area.setBackground(Color.black);
+        area.setFont(normalFont);
+        area.setEditable(false);
 
     }
 
