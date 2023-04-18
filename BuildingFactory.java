e358001a94998963efe7b65ab79bc8c8251722d1
Lin Shi <linshi1768@gmail.com>
2023-04-18 15:28:12 -0600

    factory

--- BuildingFactory.java
+++ BuildingFactory.java
@@ -1,4 +1,25 @@
 public class BuildingFactory {
 
-    
+    public Building constructBuilding(Enum.buildingType type){
+        switch (type){
+            case Smokehouse:
+                return new Smokehouse();
+            case ClothesFactory:
+                return new ClothesFactory();
+            case Bucket:
+                return new Bucket();
+            case Trap:
+                return new Trap();
+            case Hut:
+                return new Hut();
+            case Blacksmith:
+                return new Blacksmith();
+            case Mines:
+                return new Mines();
+            case Tradecart:
+                return new Tradecart();
+            default:
+                return null; 
+        }
+    }
 }
