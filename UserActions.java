30568fe592cc47b36c03fc9b7832bdf65ffa2859
Lin Shi <linshi1768@gmail.com>
2023-04-19 20:18:49 -0600

    user action update

--- UserActions.java
+++ UserActions.java
@@ -9,33 +9,31 @@ public class UserActions{
 	}
 
 	public int getFood() {
-		return random.nextInt(11) + 5; //5-15 pounds
+		return random.nextInt(3) + 1; //1-3 pounds
 	}
 
 	public int getMeat() {
-		return random.nextInt(19) + 2; //2-20 pounds
+		return random.nextInt(5) + 5; //5-10 pounds
 	}
 
 	public int getWater() {
-		return random.nextInt(10) + 1; //1-10 gallons
+		return random.nextInt(5) + 1; //1-5 gallons
 	}
 
 	public int getFur() {
-
-		//0, 1, 2 for "sheep", "bear", "lynx"
-		return random.nextInt(3);
+		return random.nextInt(3) + 1; // 1-3
 	}
 
 	public int getWood() {
-		return random.nextInt(96) + 5; //5-100 pounds
+		return random.nextInt(10) + 5; //5-15 pounds
 	}
 
 	public int getRock() {
-		return random.nextInt(196) + 5; //5-200 pounds
+		return random.nextInt(10) + 10; //10-20 pounds
 	}
 
 	public int getClothes() {
-		return random.nextInt(9) + 2; //2-19 count
+		return random.nextInt(3) + 1; //1-3 count
 	}
 
 }
